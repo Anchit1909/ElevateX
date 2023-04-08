8898c6278ede4e0c858b939258ed9e1a5d3de589
Anchit Sinha <anchit1909@gmail.com>
2023-04-08 19:59:31 +0530

    minor change in products page

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,14 +1,7 @@
 import React from "react";
 
 function products() {
-  return (
-    <div>
-      <Head>
-        <title>My Projects</title>
-      </Head>
-      
-    </div>
-  );
+  return <div></div>;
 }
 
 export default products;
