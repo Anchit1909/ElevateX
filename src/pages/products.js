10e444b23767c856dc65f811834d757b2f87b3ce
Anchit Sinha <anchit1909@gmail.com>
2023-04-08 19:54:14 +0530

    added products page

---
+++ src/pages/products.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function products() {
+  return (
+    <div>
+      <Head>
+        <title>My Projects</title>
+      </Head>
+      
+    </div>
+  );
+}
+
+export default products;
