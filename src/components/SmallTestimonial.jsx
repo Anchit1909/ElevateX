4ac8167c5b7dbc942d3f5bf509317908dd3c90bf
Akarsh <akarshsaxena28@gmail.com>
2023-04-12 10:51:10 +0530

    adding responsiveness

--- src/components/SmallTestimonial.jsx
+++ src/components/SmallTestimonial.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function SmallTestimonial() {
   return (
     <section className="bg-[#FAFBFD] mt-12 py-20">
-      <div className="text-center font-poppins max-w-screen-xl mx-auto flex flex-col items-center justify-center">
+      <div className="px-6 xl:px-0 text-center font-poppins max-w-screen-xl mx-auto flex flex-col items-center justify-center">
         <div>
-          <h1 className="text-3xl leading-10 tracking-tight max-w-6xl">
+          <h1 className="sm:text-lg md:text-2xl lg:text-3xl leading-10 tracking-tight max-w-6xl">
             “I get to talk 1-on-1 to pretty much celebrities in my industry. I
             feel really privileged to just be in touch with some of these
             people, not to mention to get business advice from them."
